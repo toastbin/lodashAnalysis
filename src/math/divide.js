2de1856618a6de4eedd6e4c89c8e6a234f83240f
toast <729440390@qq.com>
2019-11-10 00:11:35 +0800

    feature: divide multiply subtract

---
+++ src/math/divide.js
@@ -0,0 +1,18 @@
+const createMathOperation = require('../utils/createMathOperation.js')
+
+/**
+ * Divide two numbers.
+ *
+ * @since 4.7.0
+ * @category Math
+ * @param {number} dividend The first number in a division.
+ * @param {number} divisor The second number in a division.
+ * @returns {number} Returns the quotient.
+ * @example
+ *
+ * divide(6, 4)
+ * // => 1.5
+ */
+const divide = createMathOperation((dividend, divisor) => dividend / divisor, 1)
+
+module.exports = divide
