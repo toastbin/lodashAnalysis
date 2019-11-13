bfa9111cf10d81ee31ad0d09eaa681433c841fc6
toast <729440390@qq.com>
2019-11-13 22:57:03 +0800

    feature: add array.slice & toNumber

---
+++ src/array/slice.js
@@ -0,0 +1,57 @@
+/**
+ * Creates a slice of `array` from `start` up to, but not including, `end`.
+ *
+ * **Note:** This method is used instead of
+ * [`Array#slice`](https://mdn.io/Array/slice) to ensure dense arrays are
+ * returned.
+ *
+ * @since 3.0.0
+ * @category Array
+ * @param {Array} array The array to slice.
+ * @param {number} [start=0] The start position. A negative index will be treated as an offset from the end.
+ * @param {number} [end=array.length] The end position. A negative index will be treated as an offset from the end.
+ * @returns {Array} Returns the slice of `array`.
+ * @example
+ *
+ * var array = [1, 2, 3, 4]
+ *
+ * _.slice(array, 2)
+ * // => [3, 4]
+ */
+// 确保数组正确返回 
+function slice(array, start, end) {
+  // 获取 length
+  let length = array == null ? 0 : array.length
+  if (!length) {
+    return []
+  }
+  // 获取 start 和 end  并且如果没写默认为0 和 length
+  start = start == null ? 0 : start
+  end = end === undefined ? length : end
+
+  // start < 0 转成倒数第几个
+  if (start < 0) {
+    start = -start > length ? 0 : (length + start)
+  }
+
+  end = end > length ? length : end
+  if (end < 0) {
+    end += length
+  }
+  // start > end  ==> 0
+  // >>> 0  这个操作是为了让其他类型 比如 object  array  变成 0 提高代码的健壮性
+  length = start > end ? 0 : ((end - start) >>> 0)
+  start >>>= 0
+  let index = -1
+  // 返回一个新的数组引用
+  const result = new Array(length)
+  while (++index < length) {
+    // 赋值
+    result[index] = array[index + start]
+  }
+  return result
+}
+
+console.log(slice([1, 2, 3], 0, {}))
+
+module.exports = slice
---
+++ src/utils/toNumber.js
@@ -0,0 +1,77 @@
+const isObject = require('./isObject.js')
+const isSymbol = require('./isSymbol.js')
+
+/** Used as references for various `Number` constants. */
+const NAN = 0 / 0
+
+/** Used to match leading and trailing whitespace. */
+const reTrim = /^\s+|\s+$/g
+
+/** Used to detect bad signed hexadecimal string values. */
+const reIsBadHex = /^[-+]0x[0-9a-f]+$/i
+
+/** Used to detect binary string values. */
+const reIsBinary = /^0b[01]+$/i
+
+/** Used to detect octal string values. */
+const reIsOctal = /^0o[0-7]+$/i
+
+/** Built-in method references without a dependency on `root`. */
+const freeParseInt = parseInt
+
+/**
+ * Converts `value` to a number.
+ *
+ * @since 4.0.0
+ * @category Lang
+ * @param {*} value The value to process.
+ * @returns {number} Returns the number.
+ * @see isInteger, toInteger, isNumber
+ * @example
+ *
+ * toNumber(3.2)
+ * // => 3.2
+ *
+ * toNumber(Number.MIN_VALUE)
+ * // => 5e-324
+ *
+ * toNumber(Infinity)
+ * // => Infinity
+ *
+ * toNumber('3.2')
+ * // => 3.2
+ */
+function toNumber(value) {
+  if (typeof value === 'number') {
+    return value
+  }
+  if (isSymbol(value)) {
+    return NAN
+  }
+  if (isObject(value)) {
+    // {}.valueOf() => {}
+    // 区分了包装的对象类型和纯对象类型 valueOf() 返回对象的原始值
+    const other = typeof value.valueOf === 'function' ? value.valueOf() : value
+    console.log(value.valueOf(), 'value.valueOf()');
+    console.log(other, 'other', typeof other);
+    // `${ {} }` => [object Object]
+    value = isObject(other) ? `${other}` : other
+    console.log(value);
+  }
+  if (typeof value !== 'string') {
+    console.log(value, 'value');
+    return value === 0 ? value : +value
+  }
+
+  // 除空格
+  value = value.replace(reTrim, '')
+  console.log(value);
+  const isBinary = reIsBinary.test(value)
+  return (isBinary || reIsOctal.test(value))
+    ? freeParseInt(value.slice(2), isBinary ? 2 : 8)
+    : (reIsBadHex.test(value) ? NAN : +value)
+}
+
+console.log(toNumber({ a: 1 }));
+
+module.exports = toNumber
